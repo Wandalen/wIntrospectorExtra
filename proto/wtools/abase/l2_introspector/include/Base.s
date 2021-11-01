c696619b7ec92529c88121d8b19ca18c43b1b58c
dmvict <dm.vict.kr@gmail.com>
2021-11-01 09:46:11 +0200

    Fix config and include files, change type of dependency `wprocess` to development

--- proto/wtools/abase/l2_introspector/include/Base.s
+++ proto/wtools/abase/l2_introspector/include/Base.s
@@ -8,7 +8,6 @@ if( typeof module !== 'undefined' )
   const _ = require( '../../../../node_modules/Tools' );
 
   _.include( 'wIntrospectorBasic' );
-  _.include( 'wProcess' );
   _.include( 'wFilesBasic' );
 
   module[ 'exports' ] = wTools;
